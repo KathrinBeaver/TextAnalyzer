44cd487e7130d4089527dd92ed0e3d5e81ffebe7
Sergey <Sergey@DESKTOP-QB1L01V>
2017-10-31 02:07:35 +0300

    Первые попытки понять как работает сумка слов в dl4j

---
+++ src/main/java/com/mai/textanalyzer/bag_of_words/BagOfWords.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mai.textanalyzer.bag_of_words;
+
+import org.deeplearning4j.bagofwords.vectorizer.BagOfWordsVectorizer;
+
+/**
+ *
+ * @author Sergey
+ */
+public class BagOfWords extends BagOfWordsVectorizer {
+
+   
+}
---
+++ src/test/java/com/mai/textanalyzer/bag_of_words/test/TestBagOfWords.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mai.textanalyzer.bag_of_words.test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import org.datavec.api.util.ClassPathResource;
+import org.deeplearning4j.bagofwords.vectorizer.BagOfWordsVectorizer;
+import org.deeplearning4j.models.word2vec.VocabWord;
+import org.deeplearning4j.text.sentenceiterator.labelaware.LabelAwareFileSentenceIterator;
+import org.deeplearning4j.text.sentenceiterator.labelaware.LabelAwareSentenceIterator;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+import org.junit.Test;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+/**
+ *
+ * @author Sergey
+ */
+public class TestBagOfWords {
+
+    @Test
+    public void test() throws FileNotFoundException {
+        File rootDir = new ClassPathResource("testFile.txt").getFile();
+        LabelAwareSentenceIterator iter = new LabelAwareFileSentenceIterator(rootDir);
+        TokenizerFactory tokenizerFactory = new DefaultTokenizerFactory();
+        BagOfWordsVectorizer vectorizer = new BagOfWordsVectorizer.Builder()
+                .setMinWordFrequency(1)
+                .setStopWords(new ArrayList<String>())
+                .setTokenizerFactory(tokenizerFactory)
+                .setIterator(iter)
+                //                .labels(labels)
+                //                .cleanup(true)
+                .build();
+
+        vectorizer.fit();
+        VocabWord word = vectorizer.getVocabCache().wordFor("file.");
+        // vectorizer.vectorize();
+        INDArray array = vectorizer.transform("very data cat dog xzxz xz xz xz");
+        System.out.println(array);
+    }
+
+}
